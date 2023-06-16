5d1f9ade7c47b44a9980509ee9f0d8970505bb94
John Champ <72492771+john-champ@users.noreply.github.com>
2023-06-16 14:37:58 +0300

    Nuxt Starter Kit added

---
+++ README.md
@@ -0,0 +1,76 @@
+# Tailkit - Starter Kit - Nuxt
+
+This is a Nuxt Starter Kit for using Tailkit - https://tailkit.com UI components out of the box in your project.
+
+```
+Requirements: node.js
+
+Installation: npm install
+
+Development: npm run dev
+Production: npm run build
+```
+
+## Nuxt 3 Minimal Starter
+
+Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
+
+## Setup
+
+Make sure to install the dependencies:
+
+```bash
+# npm
+npm install
+
+# pnpm
+pnpm install
+
+# yarn
+yarn install
+```
+
+## Development Server
+
+Start the development server on `http://localhost:3000`:
+
+```bash
+# npm
+npm run dev
+
+# pnpm
+pnpm run dev
+
+# yarn
+yarn dev
+```
+
+## Production
+
+Build the application for production:
+
+```bash
+# npm
+npm run build
+
+# pnpm
+pnpm run build
+
+# yarn
+yarn build
+```
+
+Locally preview production build:
+
+```bash
+# npm
+npm run preview
+
+# pnpm
+pnpm run preview
+
+# yarn
+yarn preview
+```
+
+Check out the [deployment documentation](https://nuxt.com/docs/getting-started/deployment) for more information.
---
+++ tailwind.config.js
@@ -0,0 +1,94 @@
+// Tailkit (Tailwind CSS v3 Configuration)
+import plugin from 'tailwindcss/plugin';
+import defaultTheme from 'tailwindcss/defaultTheme';
+import colors from 'tailwindcss/colors';
+
+import aspectRatio from '@tailwindcss/aspect-ratio';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./composables/**/*.{js,ts}",
+    "./plugins/**/*.{js,ts}",
+    "./utils/**/*.{js,ts}",
+    "./App.{js,ts,vue}",
+    "./app.{js,ts,vue}",
+    "./Error.{js,ts,vue}",
+    "./error.{js,ts,vue}",
+    "./app.config.{js,ts"
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Inter',
+          ...defaultTheme.fontFamily.sans,
+        ]
+      },
+      maxWidth: {
+        '8xl': '90rem',
+        '9xl': '105rem',
+        '10xl': '120rem',
+      },
+      zIndex: {
+        '1': 1,
+        '60': 60,
+        '70': 70,
+        '80': 80,
+        '90': 90,
+        '100': 100,
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              textDecoration: 'none',
+              '&:hover': {
+                opacity: '.75',
+              },
+            },
+            img: {
+              borderRadius: defaultTheme.borderRadius.lg,
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    aspectRatio,
+    forms,
+    typography,
+    plugin(function({ addUtilities }) {
+      const utilFormSwitch = {
+        '.form-switch': {
+          'border': 'transparent',
+          'background-color': colors.gray[300],
+          'background-image': "url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='%23fff'/%3e%3c/svg%3e\")",
+          'background-position': 'left center',
+          'background-repeat': 'no-repeat',
+          'background-size': 'contain !important',
+          'vertical-align': 'top',
+          '&:checked': {
+            'border': 'transparent',
+            'background-color': 'currentColor',
+            'background-image': "url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='%23fff'/%3e%3c/svg%3e\")",
+            'background-position': 'right center',
+          },
+          '&:disabled, &:disabled + label': {
+            'opacity': '.5',
+            'cursor': 'not-allowed',
+          },
+        },
+      };
+
+      addUtilities(utilFormSwitch);
+    }),
+  ],
+}
