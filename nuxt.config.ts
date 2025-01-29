1db6b7ec87666672156251436779807bba253211
John Champ <72492771+john-champ@users.noreply.github.com>
2025-01-29 11:11:32 +0200

    Tailwind CSS v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  css: ["~/assets/css/tailkit.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
